074b40c14bf58a9eb125d0f6cb6cbb1fc663ffe1
K.H Lee <beta1360@naver.com>
2018-04-09 20:38:39 +0900

    Create 8958.py

---
+++ Baekjoon/8958.py
@@ -0,0 +1,17 @@
+N = int(input())
+list = []
+
+for i in range(0,N):
+    ox = input()
+    point = 0
+    isO = 0
+
+    for j in range(0, len(ox)):
+        if ox[j] == "O":
+            isO = isO + 1
+            point = point + isO
+
+        else: #if ox[j] == "X":
+            isO = 0
+
+    print(point)
