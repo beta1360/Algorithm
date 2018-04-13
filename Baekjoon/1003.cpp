3bcded7fe905e75f81c303fbe68f23a9f3b08912
K.H Lee <beta1360@naver.com>
2018-04-13 18:21:03 +0900

    Create 1003.cpp

---
+++ Baekjoon/1003.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int mem[41] = { 0 };
+
+int fibonacci(int n) {
+    if (mem[n] > 0) return mem[n];
+    if (n == 0) return 0;
+    if (n == 1 || n == 2) return 1;
+    return mem[n] = fibonacci(n - 2) + fibonacci(n - 1);
+}
+ 
+int main() {
+    int T, N;
+    scanf("%d", &T);
+ 
+    for (int i = 0; i < T; ++i) {
+        scanf("%d", &N);
+        if (N == 0) printf("1 0\n");
+        else printf("%d %d\n", fibonacci(N - 1), fibonacci(N));
+    }
+ 
+    return 0;
+}
