42bab1eb502fa5dda3dc56a540cf629511a3b30f
K.H Lee <beta1360@naver.com>
2018-07-09 16:26:08 +0900

    Create 1094.java

---
+++ Baekjoon/1094.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int length = 64;
+        int x = sc.nextInt();
+        int count = 0;
+        int sum = 0;
+ 
+        while (x > 0) {
+            if (length > x) 
+                length /= 2;
+            else {
+                count++;
+                x -= length;
+            }
+        }
+        System.out.println(count);
+        sc.close();
+    }
+}
