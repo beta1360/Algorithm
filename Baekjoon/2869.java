efa4afc4de3e948e683e45101cca9bdfc75c2403
K.H Lee <beta1360@naver.com>
2018-10-08 14:29:43 +0900

    BOJ 2869번 - 달팽이는 올라가고 싶다 - java

---
+++ Baekjoon/2869.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] argv) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
+        // V <= d*(A-B) + A
+        int day = (V-B -1) / (A-B) + 1;
+        System.out.println(day);
+
+        br.close();
+    }
+}
